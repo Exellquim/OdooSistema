50c3356a82f41260460906ef434510314551d981
Exellquim <106396189+Exellquim@users.noreply.github.com>
2024-07-09 12:05:38 -0600

    Update main.py

--- lunch_time/controllers/main.py
+++ lunch_time/controllers/main.py
@@ -1,7 +1,6 @@
 from odoo import http
 from odoo.http import request
 from datetime import datetime, date
-import pytz
 
 class LunchTime(http.Controller):
 
@@ -23,12 +22,8 @@ class LunchTime(http.Controller):
                 'error': 'Empleado no encontrado.'
             })
 
-        # Get current datetime in UTC
-        current_time_utc = datetime.utcnow()
-
-        # Convert UTC to desired timezone (e.g., 'America/Mexico_City')
-        desired_timezone = pytz.timezone('America/Mexico_City')  # Ajusta a tu zona horaria deseada
-        current_time_desired = current_time_utc.replace(tzinfo=pytz.utc).astimezone(desired_timezone)
+        # Get current datetime
+        current_time = datetime.now()
 
         # Search for today's attendance records for the employee
         attendance = request.env['hr.attendance'].sudo().search([
@@ -38,10 +33,10 @@ class LunchTime(http.Controller):
 
         if attendance:
             if attendance.hora_de_comida:
-                attendance.sudo().write({'regreso_de_comida': current_time_desired})
+                attendance.sudo().write({'regreso_de_comida': current_time})
                 message = 'Su hora de regreso ha sido registrada'
             else:
-                attendance.sudo().write({'hora_de_comida': current_time_desired})
+                attendance.sudo().write({'hora_de_comida': current_time})
                 message = 'Su hora de comida ha sido registrada'
         else:
             return request.render('lunch_time.attendance_page', {
@@ -50,6 +45,6 @@ class LunchTime(http.Controller):
 
         return request.render('lunch_time.confirmation_page', {
             'employee_name': employee.name,
-            'current_time': current_time_desired.strftime('%Y-%m-%d %H:%M:%S'),
+            'current_time': current_time.strftime('%Y-%m-%d %H:%M:%S'),
             'message': message
         })
