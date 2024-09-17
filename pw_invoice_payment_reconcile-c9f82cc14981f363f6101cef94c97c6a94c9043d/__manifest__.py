890c80e9387bb5c46b56fe596f48c2bbf4c2b503
Exellquim <106396189+Exellquim@users.noreply.github.com>
2024-09-17 11:26:11 -0600

    Update __manifest__.py

--- pw_invoice_payment_reconcile-c9f82cc14981f363f6101cef94c97c6a94c9043d/__manifest__.py
+++ pw_invoice_payment_reconcile-c9f82cc14981f363f6101cef94c97c6a94c9043d/__manifest__.py
@@ -2,18 +2,22 @@
 {
     'name': 'Multi Invoice Reconciliation | Invoice Partial Payment Reconcile Reconciliation',
     'version': '17.0.0.0.0',
-    'author': 'Preway IT Solutions',
-    'category': 'Accounting',
     'summary': 'Allows you to reconcile partial/full payments with multiple invoices/bills | Invoice Payment Reconciliation',
     'sequence': 10,
     'description': """
-This module allows you to partially or fully reconcile multiple invoices/bills during payment.
+    This module allows you to partially or fully reconcile multiple invoices/bills during payment.
     """,
+    'category': 'Accounting',
     'depends': ['account'],
     'data': [
         'security/ir.model.access.csv', 
         'views/account_payment_view.xml',  
     ],
+    'demo': [
+
+    ],
+    'installable': True,
+    'application': True,
     'price': 60.0,
     'currency': 'EUR', 
     'live_test_url': 'https://youtu.be/M9UioY72xko',  
