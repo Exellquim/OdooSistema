92b0e51e51d3d4026c5bce100ac0074c9f0ae7e6
Exellquim <106396189+Exellquim@users.noreply.github.com>
2024-09-17 10:32:21 -0600

    Update __manifest__.py

--- pw_invoice_payment_reconcile-c9f82cc14981f363f6101cef94c97c6a94c9043d/__manifest__.py
+++ pw_invoice_payment_reconcile-c9f82cc14981f363f6101cef94c97c6a94c9043d/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Multi Invoice Reconciliation | Invoice Partial Payment Reconcile Reconciliation',
-    'version': '1.0',
+    'version': '17.0.0.0.0',
     'author': 'Preway IT Solutions',
     'category': 'Accounting',
     'summary': 'Allows you to reconcile partial/full payments with multiple invoices/bills | Invoice Payment Reconciliation',
