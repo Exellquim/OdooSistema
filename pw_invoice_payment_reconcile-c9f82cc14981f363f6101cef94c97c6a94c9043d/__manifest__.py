0a70250e428b0564dcdd0269fda8939f7961daa4
Exellquim <106396189+Exellquim@users.noreply.github.com>
2024-09-17 09:54:38 -0600

    Update __manifest__.py

--- pw_invoice_payment_reconcile-c9f82cc14981f363f6101cef94c97c6a94c9043d/__manifest__.py
+++ pw_invoice_payment_reconcile-c9f82cc14981f363f6101cef94c97c6a94c9043d/__manifest__.py
@@ -1,23 +1,24 @@
 # -*- coding: utf-8 -*-
 {
-    "name": "Multi Invoice Reconciliation | Invoice Partial Payment Reconcile Reconciliation",
+    'name': 'Multi Invoice Reconciliation | Invoice Partial Payment Reconcile Reconciliation',
     'version': '1.0',
     'author': 'Preway IT Solutions',
-    'category': 'Point of Sale',
-    'depends': ['account'],
-    'summary': 'This module is allow you to reconcile payment partial/full with multiple invoice/bills on payment | Invoice Partial Payment Reconciliation | Partial Invoice Payment and Reconciliation | Invoice Reconciliation with Partial Payment | Invoice Bill Partial Payment Reconciliation | Batch Payment Reconcile',
+    'category': 'Accounting',
+    'summary': 'Allows you to reconcile partial/full payments with multiple invoices/bills | Invoice Payment Reconciliation',
     'description': """
-This module is allow you to partial/full reconcile multiple invoice/bills on payment
+This module allows you to partially or fully reconcile multiple invoices/bills during payment.
     """,
-    "data" : [
-        'security/ir.model.access.csv',
-        'views/account_payment_view.xml',
+    'depends': ['account'],  # Verifica que el módulo 'account' sea compatible con Odoo 17
+    'data': [
+        'security/ir.model.access.csv',  # Asegúrate de que los permisos sean correctos para Odoo 17
+        'views/account_payment_view.xml',  # Verifica que las vistas sean compatibles con Odoo 17
     ],
     'price': 60.0,
-    'currency': "EUR",
+    'currency': 'EUR',
     'application': True,
     'installable': True,
-    'live_test_url': 'https://youtu.be/M9UioY72xko',
-    "license": "LGPL-3",
-    "images":["static/description/Banner.png"],
+    'auto_install': False,  # Si no deseas que se instale automáticamente
+    'live_test_url': 'https://youtu.be/M9UioY72xko',  # Opcional, pero bueno incluirlo si el enlace es válido
+    'license': 'LGPL-3',  # Odoo 17 requiere especificar la licencia
+    'images': ['static/description/Banner.png'],  # Asegúrate de que la imagen esté en la ruta correcta
 }
