1518f96d958fa922540dff7c09b00557be9e5b98
Exellquim <106396189+Exellquim@users.noreply.github.com>
2024-09-17 10:12:52 -0600

    Update __manifest__.py

--- pw_invoice_payment_reconcile-c9f82cc14981f363f6101cef94c97c6a94c9043d/__manifest__.py
+++ pw_invoice_payment_reconcile-c9f82cc14981f363f6101cef94c97c6a94c9043d/__manifest__.py
@@ -8,17 +8,17 @@
     'description': """
 This module allows you to partially or fully reconcile multiple invoices/bills during payment.
     """,
-    'depends': ['account'],  # Verifica que el módulo 'account' sea compatible con Odoo 17
+    'depends': ['account'],
     'data': [
-        'security/ir.model.access.csv',  # Asegúrate de que los permisos sean correctos para Odoo 17
-        'views/account_payment_view.xml',  # Verifica que las vistas sean compatibles con Odoo 17
+        'security/ir.model.access.csv', 
+        'views/account_payment_view.xml',  
     ],
     'price': 60.0,
     'currency': 'EUR',
     'application': True,
     'installable': True,
-    'auto_install': False,  # Si no deseas que se instale automáticamente
-    'live_test_url': 'https://youtu.be/M9UioY72xko',  # Opcional, pero bueno incluirlo si el enlace es válido
+    'auto_install': False,  
+    'live_test_url': 'https://youtu.be/M9UioY72xko',  
     'license': 'LGPL-3',  # Odoo 17 requiere especificar la licencia
-    'images': ['static/description/Banner.png'],  # Asegúrate de que la imagen esté en la ruta correcta
+    'images': ['static/description/Banner.png'], 
 }
