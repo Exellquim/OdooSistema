c62ccf849a6beb890d57eb91eaf27185595fb14c
Exellquim <106396189+Exellquim@users.noreply.github.com>
2024-09-17 11:22:55 -0600

    Update __manifest__.py

--- lunch_time/__manifest__.py
+++ lunch_time/__manifest__.py
@@ -2,27 +2,30 @@
     'name': 'Lunch Time',
     'version': '17.0.0.0.0',
     'summary': 'Module for managing lunch times',
-    'description': 'This module allows employees to manage their lunch times',
     'sequence': 10,
+    'description': 'This module allows employees to manage their lunch times',
     'category': 'Human Resources',
-    'author': 'RADAXBS',
     'website': 'https://www.example.com',
     'depends': [
         'base',               
         'hr_attendance',        
-        'website',              
+        'website'            
     ],
     'data': [
-        #'views/hr_attendance_views.xml',
-        #'views/hr_attendance.xml',
+         'views/hr_attendance_views.xml',
+         'views/hr_attendance.xml',
     ],
+    'demo': [
+
+    ],
+    'installable': True,
+    'application': True,
     'assets': {
         'web.assets_frontend': [
-            #'lunch_time/static/src/css/attendance.css',  
+            # 'lunch_time/static/src/css/attendance.css',  
         ],
     },
-    'license': 'LGPL-3',  # Especifica la licencia para Odoo 17
-    
+    'license': 'LGPL-3',
 }
 
 
