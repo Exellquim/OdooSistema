b06e2a0f3ff1831d22a9118fb66b46e4bc1e27c8
Exellquim <106396189+Exellquim@users.noreply.github.com>
2024-09-17 11:06:21 -0600

    Update __manifest__.py

--- lunch_time/__manifest__.py
+++ lunch_time/__manifest__.py
@@ -3,6 +3,7 @@
     'version': '17.0.0.0.0',
     'summary': 'Module for managing lunch times',
     'description': 'This module allows employees to manage their lunch times',
+    'sequence': 10,
     'category': 'Human Resources',
     'author': 'RADAXBS',
     'website': 'https://www.example.com',
