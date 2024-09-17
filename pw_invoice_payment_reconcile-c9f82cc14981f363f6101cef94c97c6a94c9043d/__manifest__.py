f78d3385e8da5b04f246c3b76fa6c08796072029
Exellquim <106396189+Exellquim@users.noreply.github.com>
2024-09-17 11:27:46 -0600

    Update __manifest__.py

--- pw_invoice_payment_reconcile-c9f82cc14981f363f6101cef94c97c6a94c9043d/__manifest__.py
+++ pw_invoice_payment_reconcile-c9f82cc14981f363f6101cef94c97c6a94c9043d/__manifest__.py
@@ -8,7 +8,7 @@
     This module allows you to partially or fully reconcile multiple invoices/bills during payment.
     """,
     'category': 'Accounting',
-    'depends': ['account'],
+    'depends': ['base','account'],
     'data': [
         'security/ir.model.access.csv', 
         'views/account_payment_view.xml',  
