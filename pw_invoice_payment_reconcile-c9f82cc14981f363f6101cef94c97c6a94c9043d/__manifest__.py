8ff25cf4d32215ce7bb4f3eae64f11aea539626d
Exellquim <106396189+Exellquim@users.noreply.github.com>
2024-09-25 09:02:21 -0600

    Add files via upload

---
+++ pw_invoice_payment_reconcile-c9f82cc14981f363f6101cef94c97c6a94c9043d/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Multi Invoice Reconciliation | Invoice Partial Payment Reconcile Reconciliation',
+    'version': '1.0',
+    'author': 'Preway IT Solutions',
+    'category': 'Accounting',
+    'summary': 'Allows you to reconcile partial/full payments with multiple invoices/bills | Invoice Payment Reconciliation',
+    'description': """
+This module allows you to partially or fully reconcile multiple invoices/bills during payment.
+    """,
+    'depends': ['account'],
+    'data': [
+        'security/ir.model.access.csv', 
+        'views/account_payment_view.xml',  
+    ],
+    'price': 60.0,
+    'currency': 'EUR',
+    'application': True,
+    'installable': True,
+    'auto_install': False,  
+    'live_test_url': 'https://youtu.be/M9UioY72xko',  
+    'license': 'LGPL-3',  # Odoo 17 requiere especificar la licencia
+    'images': ['static/description/Banner.png'], 
+}
