e1218435589b4b0c4c30ab65ef21739f839a0018
Exellquim <106396189+Exellquim@users.noreply.github.com>
2024-10-28 12:53:41 -0600

    Update main.py

--- lunch_time/controllers/main.py
+++ lunch_time/controllers/main.py
@@ -1,6 +1,6 @@
 from odoo import http
 from odoo.http import request
-from datetime import datetime, date, timedelta
+from datetime import datetime, timedelta
 import pytz  # Importar pytz para el manejo de zonas horarias
 
 class LunchTime(http.Controller):
@@ -29,14 +29,17 @@ class LunchTime(http.Controller):
         # Convertir a la zona horaria del usuario
         tz = request.env.user.tz  # Obtener la zona horaria del usuario
         if tz:
-            user_tz = pytz.timezone(tz)  # Corregir aquí
+            user_tz = pytz.timezone(tz)
             current_time = current_time_utc.astimezone(user_tz)
         else:
             current_time = current_time_utc  # Si no hay zona horaria, usar UTC
 
+        # Convertir a naive datetime antes de guardar
+        naive_current_time = current_time.replace(tzinfo=None)
+
         # Definir el inicio y fin del día actual
-        start_of_extended_day = current_time.replace(hour=0, minute=0, second=0) - timedelta(hours=6)
-        end_of_extended_day = current_time.replace(hour=0, minute=0, second=0) + timedelta(days=1, hours=6)
+        start_of_extended_day = naive_current_time.replace(hour=0, minute=0, second=0) - timedelta(hours=6)
+        end_of_extended_day = naive_current_time.replace(hour=0, minute=0, second=0) + timedelta(days=1, hours=6)
 
         # Buscar registros de asistencia
         attendance = request.env['hr.attendance'].sudo().search([
@@ -47,10 +50,10 @@ class LunchTime(http.Controller):
 
         if attendance:
             if attendance.hora_de_comida:
-                attendance.sudo().write({'regreso_de_comida': current_time})
+                attendance.sudo().write({'regreso_de_comida': naive_current_time})
                 message = 'Su hora de regreso ha sido registrada'
             else:
-                attendance.sudo().write({'hora_de_comida': current_time})
+                attendance.sudo().write({'hora_de_comida': naive_current_time})
                 message = 'Su hora de comida ha sido registrada'
         else:
             return request.render('lunch_time.attendance_page', {
@@ -59,6 +62,6 @@ class LunchTime(http.Controller):
 
         return request.render('lunch_time.confirmation_page', {
             'employee_name': employee.name,
-            'current_time': current_time.strftime('%Y-%m-%d %H:%M:%S'),
+            'current_time': naive_current_time.strftime('%Y-%m-%d %H:%M:%S'),
             'message': message
         })
