a86ed21f4e175c3b4b76b757313d375f1dd4a42c
Exellquim <106396189+Exellquim@users.noreply.github.com>
2024-12-18 11:09:48 -0600

    Delete lunch_time directory

--- lunch_time/models/hr_attendance.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class HrAttendance(models.Model):
-    _inherit = 'hr.attendance'
-
-    hora_de_comida = fields.Datetime(string="Hora de Comida")
-    regreso_de_comida = fields.Datetime(string="Regreso de Comida")
-
-
