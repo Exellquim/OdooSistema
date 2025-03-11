1bc13aaedf52756a81b457c17cfa2c4f858ce65d
Exellquim <106396189+Exellquim@users.noreply.github.com>
2025-03-11 14:48:40 -0600

    Update main.py

--- lunch_time/controllers/main.py
+++ lunch_time/controllers/main.py
@@ -2,7 +2,6 @@ from odoo import http
 from odoo.http import request
 from datetime import datetime
 import pytz  # Importar pytz para el manejo de zonas horarias
-from pytz import timezone, UTC
 
 class LunchTime(http.Controller):
 
@@ -28,28 +27,25 @@ class LunchTime(http.Controller):
         tz = pytz.timezone('America/Mexico_City')
 
         # Obtener la hora actual en la zona horaria de México correctamente
-        current_time_mx = datetime.now().astimezone(tz)
-
-        # Convertir a naive datetime antes de guardar
-        naive_current_time = current_time_mx.replace(tzinfo=None)
+        current_time_mx = datetime.now(tz)
 
         # Definir el inicio y fin del día actual en la zona horaria de México
-        start_of_day_mx = naive_current_time.replace(hour=0, minute=0, second=0, microsecond=0)
-        end_of_day_mx = naive_current_time.replace(hour=23, minute=59, second=59, microsecond=999999)
+        start_of_day_mx = current_time_mx.replace(hour=0, minute=0, second=0, microsecond=0)
+        end_of_day_mx = current_time_mx.replace(hour=23, minute=59, second=59, microsecond=999999)
 
         # Buscar registros de asistencia dentro del día en horario de México
         attendance = request.env['hr.attendance'].sudo().search([
             ('employee_id', '=', employee.id),
-            ('check_in', '>=', start_of_day_mx),
-            ('check_in', '<=', end_of_day_mx)
+            ('check_in', '>=', start_of_day_mx.replace(tzinfo=None)),
+            ('check_in', '<=', end_of_day_mx.replace(tzinfo=None))
         ], limit=1)
 
         if attendance:
             if attendance.hora_de_comida:
-                attendance.sudo().write({'regreso_de_comida': current_time_mx})
+                attendance.sudo().write({'regreso_de_comida': current_time_mx.replace(tzinfo=None)})
                 message = 'Su hora de regreso ha sido registrada'
             else:
-                attendance.sudo().write({'hora_de_comida': current_time_mx})
+                attendance.sudo().write({'hora_de_comida': current_time_mx.replace(tzinfo=None)})
                 message = 'Su hora de comida ha sido registrada'
         else:
             return request.render('lunch_time.attendance_page', {
