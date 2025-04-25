d0d6815b703c50c5857b78f3d131d65e48652e95
Exellquim <106396189+Exellquim@users.noreply.github.com>
2025-04-25 11:24:57 -0600

    Add files via upload

---
+++ custom_stock_selection_report/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Stock Picking Product Selection Report',
+    'version': '1.0',
+    'depends': ['stock'],
+    'category': 'Inventory',
+    'summary': 'Permite seleccionar productos del picking y generar un reporte',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/stock_picking_views.xml',
+        'views/picking_product_select_wizard_views.xml',
+        'reports/report.xml',
+        'reports/product_selected_template.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
---
+++ custom_stock_selection_report/wizards/__init__.py
@@ -0,0 +1,2 @@
+from . import picking_product_select_wizard
+
