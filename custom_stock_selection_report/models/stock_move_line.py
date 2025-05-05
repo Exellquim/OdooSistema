f1967dad3baf514574db3cf45a225e37d7c5e3e2
Exellquim <106396189+Exellquim@users.noreply.github.com>
2025-05-05 10:41:30 -0600

    Add files via upload

---
+++ custom_stock_selection_report/models/stock_move_line.py
@@ -0,0 +1,44 @@
+from odoo import models
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    def _prepare_stock_move_vals(self, picking, price_unit, product_uom_qty, product_uom):
+        self.ensure_one()
+        self._check_orderpoint_picking_type()
+        product = self.product_id.with_context(lang=self.order_id.dest_address_id.lang or self.env.user.lang)
+        location_dest = self.env['stock.location'].browse(self.order_id._get_destination_location())
+        location_final = self.location_final_id or self.order_id._get_final_location_record()
+        if location_final and location_final._child_of(location_dest):
+            location_dest = location_final
+        date_planned = self.date_planned or self.order_id.date_planned
+
+        vals = {
+            'name': (self.product_id.display_name or '')[:2000],
+            'product_id': self.product_id.id,
+            'date': date_planned,
+            'date_deadline': date_planned,
+            'location_id': self.order_id.partner_id.property_stock_supplier.id,
+            'location_dest_id': location_dest.id,
+            'location_final_id': location_final.id,
+            'picking_id': picking.id,
+            'partner_id': self.order_id.dest_address_id.id,
+            'move_dest_ids': [(4, x) for x in self.move_dest_ids.ids],
+            'state': 'draft',
+            'purchase_line_id': self.id,
+            'company_id': self.order_id.company_id.id,
+            'price_unit': price_unit,
+            'picking_type_id': self.order_id.picking_type_id.id,
+            'group_id': self.order_id.group_id.id,
+            'origin': self.order_id.name,
+            'description_picking': product.description_pickingin or self.name,
+            'propagate_cancel': self.propagate_cancel,
+            'warehouse_id': self.order_id.picking_type_id.warehouse_id.id,
+            'product_uom_qty': product_uom_qty,
+            'product_uom': product_uom.id,
+            'product_packaging_id': self.product_packaging_id.id,
+            'sequence': self.sequence,
+            'quantity_done': 0.0,  # <-- Agregado aquí
+        }
+
+        return vals
