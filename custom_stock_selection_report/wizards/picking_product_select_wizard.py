7fd647ca17e2d50c4adbe65ec517114f8f2cfdb6
Exellquim <106396189+Exellquim@users.noreply.github.com>
2025-05-08 12:54:34 -0600

    Update picking_product_select_wizard.py

--- custom_stock_selection_report/wizards/picking_product_select_wizard.py
+++ custom_stock_selection_report/wizards/picking_product_select_wizard.py
@@ -6,15 +6,31 @@ class PickingProductSelectWizard(models.TransientModel):
 
     picking_id = fields.Many2one('stock.picking', string='Albarán', required=True)
     product_ids = fields.Many2many('product.product', string='Productos')
+    product_qty_ids = fields.One2many('picking.product.qty', 'wizard_id', string="Cantidades de Productos")
 
     @api.model
     def default_get(self, fields):
         res = super().default_get(fields)
         if 'default_picking_id' in self.env.context:
             picking = self.env['stock.picking'].browse(self.env.context['default_picking_id'])
-            res['product_ids'] = [(6, 0, picking.move_ids_without_package.mapped('product_id').ids)]
+            # Obtener los productos y sus cantidades de move_ids_without_package
+            product_qtys = []
+            for move in picking.move_ids_without_package:
+                product_qtys.append((0, 0, {
+                    'product_id': move.product_id.id,
+                    'product_qty': move.product_uom_qty
+                }))
+            res['product_qty_ids'] = product_qtys
         return res
-
     def action_print_selected_products(self):
         self.ensure_one()
         return self.env.ref('custom_stock_selection_report.action_report_selected_products').report_action(self)
+
+class PickingProductQty(models.Model):
+    _name = 'picking.product.qty'
+    _description = 'Cantidad de producto en picking'
+
+    wizard_id = fields.Many2one('picking.product.select.wizard', string='Wizard')
+    product_id = fields.Many2one('product.product', string='Producto', required=True)
+    product_qty = fields.Float(string='Cantidad', required=True)
+
