cf51d89cc311b2490a1d6887129afebefa9dea80
Exellquim <106396189+Exellquim@users.noreply.github.com>
2025-06-23 10:01:22 -0600

    Update stock_move.py

--- lunch_time/models/stock_move.py
+++ lunch_time/models/stock_move.py
@@ -5,7 +5,7 @@ class StockMove(models.Model):
     _inherit = 'stock.move'
 
     cantidad = fields.Float(string="Cantidad para validar")
-    
+
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
@@ -13,7 +13,11 @@ class StockPicking(models.Model):
         for picking in self:
             for move in picking.move_ids_without_package:
                 if move.cantidad <= 0:
-                    raise ValidationError(f"La línea del producto '{move.product_id.display_name}' tiene cantidad en cero. No se puede validar.")
-                move.quantity = move.cantidad  # copiar el valor a quantity_done
+                    raise ValidationError(
+                        f"La línea del producto '{move.product_id.display_name}' tiene cantidad en cero. No se puede validar."
+                    )
+                # Asignar la cantidad validada correctamente
+                move.quantity_done = move.cantidad
 
-        return super().button_validate()
+        # Llamar al proceso estándar solo si todo está validado correctamente
+        return super(StockPicking, self).button_validate()
