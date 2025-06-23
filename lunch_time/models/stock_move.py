66bd44ac77efbfe49712ff0fa676da80ac3e6a22
Exellquim <106396189+Exellquim@users.noreply.github.com>
2025-06-23 16:46:22 -0600

    Update stock_move.py

--- lunch_time/models/stock_move.py
+++ lunch_time/models/stock_move.py
@@ -15,7 +15,7 @@ class StockMove(models.Model):
     @api.onchange('cantidad')
     def _onchange_cantidad(self):
         for record in self:
-            if record.product_id.tracking in ['lot', 'serial'] and not record.lot_id:
+            if record.product_id.tracking in ['lot', 'serial'] and not record.lot_ids:
                 # Si el producto requiere lote y aún no se capturó, no asignar
                 record.quantity = 0.0
             else:
