4e95de843eee8b95d05e949cf88c48fa9b73adcb
Exellquim <106396189+Exellquim@users.noreply.github.com>
2025-06-23 14:17:15 -0600

    Update stock_move.py

--- lunch_time/models/stock_move.py
+++ lunch_time/models/stock_move.py
@@ -3,19 +3,19 @@ from odoo import models, fields, api
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
-    x_studio_nuevo = fields.Char(
+    nuevo = fields.Char(
         string='Campo Nuevo',
         compute='_compute_x_studio_fields',
         store=True
     )
-    quantity = fields.Float(
+    cantidad = fields.Float(
         compute='_compute_x_studio_fields',
         store=True
     )
 
-    @api.depends('x_studio_cantidad')
+    @api.depends('cantidad')
     def _compute_x_studio_fields(self):
         for record in self:
             # Asegura que siempre se asignen los valores computados
-            record.x_studio_nuevo = ''
+            record.nuevo = ''
             record.quantity = record.x_studio_cantidad or 0.0
