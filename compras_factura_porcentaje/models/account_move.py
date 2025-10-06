d87722cbc9d1fdc09fb91ceaa1eac34d38a32a92
Soporte <140537906+SoporteRadax01@users.noreply.github.com>
2025-10-06 16:57:16 -0600

    Update account_move.py

--- compras_factura_porcentaje/models/account_move.py
+++ compras_factura_porcentaje/models/account_move.py
@@ -4,14 +4,15 @@ class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
     def _prepare_invoice_line_from_po_line(self, line, invoice_id):
-        """Personaliza la creación de líneas de factura desde la OC"""
+        """Ajusta la cantidad facturada como porcentaje del subtotal de la OC"""
         res = super()._prepare_invoice_line_from_po_line(line, invoice_id)
-   
-        if res.get("price_unit") and res.get("price_subtotal"):
-            try:
-                qty_calc = res["price_subtotal"] / res["price_unit"]
-                if 0 < qty_calc < res.get("quantity", 1):
-                    res["quantity"] = round(qty_calc, 4)
-            except ZeroDivisionError:
-                pass
+
+        subtotal_oc = line.price_unit * line.product_qty
+
+        subtotal_factura = res.get("price_unit", 0) * res.get("quantity", 0)
+
+        if subtotal_oc > 0 and subtotal_factura > 0:
+            qty_calc = subtotal_factura / subtotal_oc * line.product_qty
+            res["quantity"] = round(qty_calc, 4)
+
         return res
