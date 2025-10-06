b4ea49c1233a5caccef5cc098fc3fc8267150ad7
Soporte <140537906+SoporteRadax01@users.noreply.github.com>
2025-10-06 16:18:52 -0600

    Add files via upload

---
+++ compras_factura_porcentaje/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Compras cantidad % de lo facturado",
+    "version": "17.0.1.0.0",
+    "summary": "Ajusta automáticamente la cantidad facturada según el importe",
+    "category": "Purchases",
+    "author": "RADAX-HEPL2025",
+    "depends": ["purchase", "account"],
+    "data": [],
+    "installable": True,
+    "application": False,
+}
---
+++ compras_factura_porcentaje/models/website.py
@@ -0,0 +1,19 @@
+from odoo import api, models
+
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
+
+    @api.onchange('price_unit', 'price_subtotal')
+    def _onchange_price_subtotal_adjust_qty(self):
+        """
+        Cuando se edita el subtotal en la factura,
+        recalcula la cantidad en base al precio unitario
+        de la orden de compra vinculada.
+        """
+        for line in self:
+            if line.move_id.move_type in ['in_invoice'] and line.purchase_line_id:
+                price_unit = line.price_unit
+                subtotal = line.price_subtotal
+                if price_unit and subtotal:
+                    new_qty = subtotal / price_unit
+                    line.quantity = new_qty
