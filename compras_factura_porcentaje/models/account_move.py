95d6732aaad581c124162971563f248474d10588
Soporte <140537906+SoporteRadax01@users.noreply.github.com>
2025-10-06 16:31:55 -0600

    Update account_move.py

--- compras_factura_porcentaje/models/account_move.py
+++ compras_factura_porcentaje/models/account_move.py
@@ -1,20 +1,31 @@
-from odoo import api, models
+from odoo import models
 
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
-    @api.onchange('price_unit', 'price_subtotal')
-    def _onchange_price_subtotal_adjust_qty(self):
-        """
-        Cuando se edita el subtotal en la factura,
-        recalcula la cantidad en base al precio unitario
-        de la orden de compra vinculada.
-        """
-        for line in self:
-            if line.move_id.move_type in ['in_invoice'] and line.purchase_line_id:
-                price_unit = line.price_unit
-                subtotal = line.price_subtotal
-                if price_unit and subtotal:
-                    new_qty = subtotal / price_unit
-                    line.quantity = new_qty
+    @classmethod
+    def create(cls, vals_list):
+        records = super().create(vals_list)
+        for line in records:
+            if (
+                line.move_id.move_type == "in_invoice"
+                and line.purchase_line_id
+                and line.price_unit
+                and line.price_subtotal
+            ):
+                new_qty = line.price_subtotal / line.price_unit
+                line.quantity = new_qty
+        return records
 
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    def action_post(self):
+        for move in self:
+            if move.move_type == "in_invoice":
+                for line in move.invoice_line_ids.filtered(lambda l: l.purchase_line_id):
+                    if line.price_unit and line.price_subtotal:
+                        new_qty = line.price_subtotal / line.price_unit
+                        line.quantity = new_qty
+        return super().action_post()
