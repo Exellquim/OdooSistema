6f9a17bc6aab688f2bf258c6a67a509cf9983d5e
Soporte <140537906+SoporteRadax01@users.noreply.github.com>
2025-10-06 16:48:06 -0600

    Update account_move.py

--- compras_factura_porcentaje/models/account_move.py
+++ compras_factura_porcentaje/models/account_move.py
@@ -1,8 +1,9 @@
-from odoo import models
+from odoo import models, api
 
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
+    @api.model_create_multi
     def create(self, vals_list):
         records = super().create(vals_list)
         for line in records:
@@ -10,13 +11,15 @@ class AccountMoveLine(models.Model):
                 line.move_id.move_type == "in_invoice"
                 and line.purchase_line_id
                 and line.price_unit
-                and line.price_subtotal
+                and line.move_id.purchase_vendor_bill_id
             ):
-                try:
-                    new_qty = line.price_subtotal / line.price_unit
-                    line.quantity = round(new_qty, 4)  # redondeo para evitar decimales largos
-                except ZeroDivisionError:
-                    pass
+                po_line = line.purchase_line_id
+                total_po = po_line.price_unit * po_line.product_qty
+                total_invoiced = sum(line.mapped("purchase_line_id.invoice_lines.price_subtotal"))
+                pendiente = total_po - total_invoiced
+
+                if line.price_subtotal and line.price_subtotal < po_line.price_unit:
+                    line.quantity = line.price_subtotal / line.price_unit
         return records
 
 
@@ -28,9 +31,7 @@ class AccountMove(models.Model):
             if move.move_type == "in_invoice":
                 for line in move.invoice_line_ids.filtered(lambda l: l.purchase_line_id):
                     if line.price_unit and line.price_subtotal:
-                        try:
-                            new_qty = line.price_subtotal / line.price_unit
-                            line.quantity = round(new_qty, 4)
-                        except ZeroDivisionError:
-                            pass
+                        qty_calc = line.price_subtotal / line.price_unit
+                        if 0 < qty_calc < line.quantity:
+                            line.quantity = round(qty_calc, 4)
         return super().action_post()
