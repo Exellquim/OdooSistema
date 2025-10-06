a9dd8695db915fc11f40a347e74a7ff386a9ffc9
Soporte <140537906+SoporteRadax01@users.noreply.github.com>
2025-10-06 17:53:25 -0600

    Update account_move.py

--- compras_factura_porcentaje/models/account_move.py
+++ compras_factura_porcentaje/models/account_move.py
@@ -4,10 +4,8 @@ class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
     def _prepare_invoice_line_from_po_line(self, line, invoice_id):
-        """Ajusta la cantidad facturada según el precio unitario de la OC"""
+        """Ajusta la cantidad facturada manteniendo el precio unitario ingresado"""
         res = super()._prepare_invoice_line_from_po_line(line, invoice_id)
-
-        res["price_unit"] = line.price_unit  
 
         subtotal_oc = line.price_unit * line.product_qty
         subtotal_factura = res.get("price_unit", 0) * res.get("quantity", 0)
@@ -24,19 +22,16 @@ class AccountMoveLine(models.Model):
 
     @api.onchange('price_unit', 'quantity')
     def _onchange_price_unit_quantity(self):
-        """Corrige la cantidad según el subtotal de la OC, manteniendo el precio unitario de la OC"""
+        """Si el usuario cambia el precio, recalcula la cantidad proporcional al subtotal"""
         for line in self:
             if (
                 line.move_id.move_type == "in_invoice"
                 and line.purchase_line_id
             ):
                 po_line = line.purchase_line_id
-                price_oc = po_line.price_unit
-                subtotal_oc = price_oc * po_line.product_qty
+                subtotal_oc = po_line.price_unit * po_line.product_qty
 
                 subtotal_factura = line.price_unit * line.quantity
-
-                line.price_unit = price_oc
 
                 if subtotal_oc > 0 and subtotal_factura > 0:
                     qty_calc = subtotal_factura / subtotal_oc * po_line.product_qty
