741eb8fe8eb3a732aa58ee609da9d607590af37d
Soporte <140537906+SoporteRadax01@users.noreply.github.com>
2025-10-06 17:58:11 -0600

    Update account_move.py

--- compras_factura_porcentaje/models/account_move.py
+++ compras_factura_porcentaje/models/account_move.py
@@ -7,12 +7,12 @@ class PurchaseOrder(models.Model):
         """Ajusta la cantidad facturada manteniendo el precio unitario ingresado"""
         res = super()._prepare_invoice_line_from_po_line(line, invoice_id)
 
-        subtotal_oc = line.price_unit * line.product_qty
+        #subtotal_oc = line.price_unit * line.product_qty
         subtotal_factura = res.get("price_unit", 0) * res.get("quantity", 0)
 
-        if subtotal_oc > 0 and subtotal_factura > 0:
-            qty_calc = subtotal_factura / subtotal_oc * line.product_qty
-            res["quantity"] = round(qty_calc, 4)
+        if subtotal_factura > 0:
+         qty_calc = subtotal_factura / line.price_unit * line.product_qty
+         res["quantity"] = round(qty_calc, 4)
 
         return res
 
@@ -29,10 +29,11 @@ class AccountMoveLine(models.Model):
                 and line.purchase_line_id
             ):
                 po_line = line.purchase_line_id
-                subtotal_oc = po_line.price_unit * po_line.product_qty
+                #subtotal_oc = po_line.price_unit * po_line.product_qty
 
                 subtotal_factura = line.price_unit * line.quantity
 
-                if subtotal_oc > 0 and subtotal_factura > 0:
-                    qty_calc = subtotal_factura / subtotal_oc * po_line.product_qty
+                if subtotal_factura > 0:
+                    qty_calc = subtotal_factura / po_line.price_unit * po_line.product_qty
                     line.quantity = round(qty_calc, 4)
+
