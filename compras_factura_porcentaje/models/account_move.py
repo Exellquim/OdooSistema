b86304da7515524100d1c7fae3e5c1ea79b6e58d
Soporte <140537906+SoporteRadax01@users.noreply.github.com>
2025-10-06 18:05:58 -0600

    Update account_move.py

--- compras_factura_porcentaje/models/account_move.py
+++ compras_factura_porcentaje/models/account_move.py
@@ -1,40 +1,24 @@
-from odoo import models, api
+from odoo import models
 
-class PurchaseOrder(models.Model):
-    _inherit = "purchase.order"
+class PurchaseOrderLine(models.Model):
+    _inherit = "purchase.order.line"
 
-    def _prepare_invoice_line_from_po_line(self, line, invoice_id):
-        """Ajusta cantidad proporcional manteniendo precio ingresado en factura"""
-        res = super()._prepare_invoice_line_from_po_line(line, invoice_id)
+    def _get_invoice_qty(self):
+        """Ajusta la cantidad facturada en la OC según el subtotal vs precio OC"""
+        self.ensure_one()
+        qty = super()._get_invoice_qty()
 
-        price_oc = line.price_unit
-        qty_oc = line.product_qty
-        price_factura = res.get("price_unit", 0)
-
-        if price_oc > 0 and price_factura > 0:
-            # calcular proporción según precio facturado vs OC
-            factor = price_factura / price_oc
-            res["quantity"] = round(qty_oc * factor, 4)
-
-        return res
-
-
-class AccountMoveLine(models.Model):
-    _inherit = "account.move.line"
-
-    @api.onchange('price_unit')
-    def _onchange_price_unit(self):
-        """Recalcula cantidad cuando se cambia el precio, tomando como referencia la OC"""
         for line in self:
-            if (
-                line.move_id.move_type == "in_invoice"
-                and line.purchase_line_id
-            ):
-                po_line = line.purchase_line_id
-                price_oc = po_line.price_unit
-                qty_oc = po_line.product_qty
-                price_factura = line.price_unit
+            for inv_line in line.invoice_lines.filtered(lambda l: l.move_id.move_type == "in_invoice"):
+                # Tomar precio y subtotal de la factura
+                price_factura = inv_line.price_unit
+                subtotal_factura = inv_line.price_subtotal
 
-                if price_oc > 0 and price_factura > 0:
-                    factor = price_factura / price_oc
-                    line.quantity = round(qty_oc * factor, 4)
+                # Precio unitario de la OC
+                price_oc = line.price_unit
+
+                if price_oc > 0:
+                    # Ajustar cantidad proporcional según el subtotal facturado
+                    qty = subtotal_factura / price_oc
+
+        return qty
