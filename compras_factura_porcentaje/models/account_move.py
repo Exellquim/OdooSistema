15f4de2030cba0a6dd4c77aa9bc243bad8689e68
Soporte <140537906+SoporteRadax01@users.noreply.github.com>
2025-10-06 18:02:16 -0600

    Update account_move.py

--- compras_factura_porcentaje/models/account_move.py
+++ compras_factura_porcentaje/models/account_move.py
@@ -4,15 +4,17 @@ class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
     def _prepare_invoice_line_from_po_line(self, line, invoice_id):
-        """Ajusta la cantidad facturada manteniendo el precio unitario ingresado"""
+        """Ajusta cantidad proporcional manteniendo precio ingresado en factura"""
         res = super()._prepare_invoice_line_from_po_line(line, invoice_id)
 
-        #subtotal_oc = line.price_unit * line.product_qty
-        subtotal_factura = res.get("price_unit", 0) * res.get("quantity", 0)
+        price_oc = line.price_unit
+        qty_oc = line.product_qty
+        price_factura = res.get("price_unit", 0)
 
-        if subtotal_factura > 0:
-         qty_calc = subtotal_factura / line.price_unit * line.product_qty
-         res["quantity"] = round(qty_calc, 4)
+        if price_oc > 0 and price_factura > 0:
+            # calcular proporción según precio facturado vs OC
+            factor = price_factura / price_oc
+            res["quantity"] = round(qty_oc * factor, 4)
 
         return res
 
@@ -20,20 +22,19 @@ class PurchaseOrder(models.Model):
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
-    @api.onchange('price_unit', 'quantity')
-    def _onchange_price_unit_quantity(self):
-        """Si el usuario cambia el precio, recalcula la cantidad proporcional al subtotal"""
+    @api.onchange('price_unit')
+    def _onchange_price_unit(self):
+        """Recalcula cantidad cuando se cambia el precio, tomando como referencia la OC"""
         for line in self:
             if (
                 line.move_id.move_type == "in_invoice"
                 and line.purchase_line_id
             ):
                 po_line = line.purchase_line_id
-                #subtotal_oc = po_line.price_unit * po_line.product_qty
+                price_oc = po_line.price_unit
+                qty_oc = po_line.product_qty
+                price_factura = line.price_unit
 
-                subtotal_factura = line.price_unit * line.quantity
-
-                if subtotal_factura > 0:
-                    qty_calc = subtotal_factura / po_line.price_unit * po_line.product_qty
-                    line.quantity = round(qty_calc, 4)
-
+                if price_oc > 0 and price_factura > 0:
+                    factor = price_factura / price_oc
+                    line.quantity = round(qty_oc * factor, 4)
