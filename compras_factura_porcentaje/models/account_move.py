af1b8a46b019cb2599a317a39188d5d95ef9021a
Soporte <140537906+SoporteRadax01@users.noreply.github.com>
2025-10-08 13:19:10 -0600

    Update account_move.py

--- compras_factura_porcentaje/models/account_move.py
+++ compras_factura_porcentaje/models/account_move.py
@@ -12,8 +12,8 @@ class PurchaseOrderLine(models.Model):
     @api.depends('invoice_lines.move_id.state', 'invoice_lines.price_subtotal')
     def _compute_qty_invoiced(self):
         """
-        - qty_invoiced = cantidad proporcional facturada (ej: 0.5 de 1)
-        - porcentaje_invoiced = % del total facturado (ej: 0.5)
+        - qty_invoiced = cantidad proporcional facturada
+        - porcentaje_invoiced = % del total facturado
         """
         super()._compute_qty_invoiced()
 
@@ -29,10 +29,25 @@ class PurchaseOrderLine(models.Model):
             if total_facturado > 0 and total_oc > 0 and line.price_unit > 0:
                 porcentaje = total_facturado / total_oc
                 line.porcentaje_invoiced = round(porcentaje, 4)
-
-                # cantidad facturada proporcional
-                qty_facturada = line.product_qty * porcentaje
-                line.qty_invoiced = min(qty_facturada, line.product_qty)
+                line.qty_invoiced = min(line.product_qty * porcentaje, line.product_qty)
             else:
                 line.porcentaje_invoiced = 0.0
                 line.qty_invoiced = 0.0
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    def _get_invoiceable_lines(self, final=False):
+        """
+        Permitir facturación parcial basada en porcentaje,
+        aunque la política del producto sea 'cantidad recibida'.
+        """
+        res = super()._get_invoiceable_lines(final=final)
+        for order in self:
+            for line in order.order_line:
+                # Si la línea aún no está facturada por completo, la hacemos facturable
+                if line.product_id and line.qty_invoiced < line.product_qty:
+                    if line not in res:
+                        res |= line
+        return res
