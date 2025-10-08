4b897facbbdae5062baa27b562140645bce447da
Soporte <140537906+SoporteRadax01@users.noreply.github.com>
2025-10-08 13:01:42 -0600

    Update account_move.py

--- compras_factura_porcentaje/models/account_move.py
+++ compras_factura_porcentaje/models/account_move.py
@@ -5,13 +5,18 @@ class PurchaseOrderLine(models.Model):
 
     porcentaje_invoiced = fields.Float(
         string="Porcentaje Facturado",
-        compute="_compute_porcentaje_invoiced",
-        store=False   # <--- no crea columna en DB
+        compute="_compute_qty_invoiced",
+        store=False
     )
 
     @api.depends('invoice_lines.move_id.state', 'invoice_lines.price_subtotal')
-    def _compute_porcentaje_invoiced(self):
-        """Calcula el % facturado solo de forma informativa"""
+    def _compute_qty_invoiced(self):
+        """
+        - qty_invoiced = cantidad proporcional facturada (ej: 0.5 de 1)
+        - porcentaje_invoiced = % del total facturado (ej: 0.5)
+        """
+        super()._compute_qty_invoiced()
+
         for line in self:
             total_oc = line.price_unit * line.product_qty
             total_facturado = sum(
@@ -20,7 +25,14 @@ class PurchaseOrderLine(models.Model):
                 if inv_line.move_id.state != 'cancel'
                 and inv_line.move_id.move_type == 'in_invoice'
             )
-            if total_facturado > 0 and total_oc > 0:
-                line.porcentaje_invoiced = round(total_facturado / total_oc, 4)
+
+            if total_facturado > 0 and total_oc > 0 and line.price_unit > 0:
+                porcentaje = total_facturado / total_oc
+                line.porcentaje_invoiced = round(porcentaje, 4)
+
+                # cantidad facturada proporcional
+                qty_facturada = line.product_qty * porcentaje
+                line.qty_invoiced = min(qty_facturada, line.product_qty)
             else:
                 line.porcentaje_invoiced = 0.0
+                line.qty_invoiced = 0.0
