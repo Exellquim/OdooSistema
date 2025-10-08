b30dd7f4d60d9c4104b392cf2aa9bc8c8662aee1
Soporte <140537906+SoporteRadax01@users.noreply.github.com>
2025-10-08 12:20:05 -0600

    Update account_move.py

--- compras_factura_porcentaje/models/account_move.py
+++ compras_factura_porcentaje/models/account_move.py
@@ -1,35 +1,27 @@
-from odoo import models, api
+from odoo import models, fields, api
 
 class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
 
-    def _prepare_account_move_line(self, move):
-        """
-        Ajusta la línea de factura:
-        - Cantidad proporcional (ejemplo: 0.50)
-        - Precio unitario = igual al de la OC
-        - Subtotal proporcional
-        """
-        res = super()._prepare_account_move_line(move)
+    porcentaje_invoiced = fields.Float(
+        string="Porcentaje Facturado",
+        compute="_compute_porcentaje_invoiced",
+        store=False   # <--- solo calculado en pantalla
+    )
 
-        if move.move_type == 'in_invoice' and self.product_qty > 0 and self.price_unit > 0:
-            total_oc = self.price_unit * self.product_qty
-
-            # Total ya facturado
+    @api.depends('invoice_lines.move_id.state', 'invoice_lines.price_subtotal')
+    def _compute_porcentaje_invoiced(self):
+        """Calcula el % facturado solo de forma informativa"""
+        for line in self:
+            total_oc = line.price_unit * line.product_qty
             total_facturado = sum(
                 inv_line.price_subtotal
-                for inv_line in self.invoice_lines
+                for inv_line in line.invoice_lines
                 if inv_line.move_id.state != 'cancel'
                 and inv_line.move_id.move_type == 'in_invoice'
             )
 
-            pendiente = total_oc - total_facturado
-            if pendiente > 0:
-                # cantidad proporcional pendiente
-                qty_pendiente = pendiente / self.price_unit
-
-                res.update({
-                    "quantity": qty_pendiente,
-                    "price_unit": self.price_unit,  # el mismo de la OC
-                })
-        return res
+            if total_facturado > 0 and total_oc > 0:
+                line.porcentaje_invoiced = round(total_facturado / total_oc, 4)
+            else:
+                line.porcentaje_invoiced = 0.0
