094e1de6a799e3e7f2e6465bba3ba33a93c7d49c
Soporte <140537906+SoporteRadax01@users.noreply.github.com>
2025-10-08 12:26:24 -0600

    Update account_move.py

--- compras_factura_porcentaje/models/account_move.py
+++ compras_factura_porcentaje/models/account_move.py
@@ -1,27 +1,35 @@
-from odoo import models, fields, api
+from odoo import models, api
 
 class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
 
-    porcentaje_invoiced = fields.Float(
-        string="Porcentaje Facturado",
-        compute="_compute_porcentaje_invoiced",
-        store=False   # <--- solo calculado en pantalla
-    )
+    def _prepare_account_move_line(self, move):
+        """
+        Ajusta la línea de factura para facturación parcial:
+        - Cantidad proporcional
+        - Precio unitario = el de la OC (nunca cambia)
+        - Subtotal proporcional
+        """
+        res = super()._prepare_account_move_line(move)
 
-    @api.depends('invoice_lines.move_id.state', 'invoice_lines.price_subtotal')
-    def _compute_porcentaje_invoiced(self):
-        """Calcula el % facturado solo de forma informativa"""
-        for line in self:
-            total_oc = line.price_unit * line.product_qty
+        if move.move_type == 'in_invoice' and self.product_qty > 0 and self.price_unit > 0:
+            total_oc = self.price_unit * self.product_qty
+
+            # Total ya facturado
             total_facturado = sum(
                 inv_line.price_subtotal
-                for inv_line in line.invoice_lines
+                for inv_line in self.invoice_lines
                 if inv_line.move_id.state != 'cancel'
                 and inv_line.move_id.move_type == 'in_invoice'
             )
 
-            if total_facturado > 0 and total_oc > 0:
-                line.porcentaje_invoiced = round(total_facturado / total_oc, 4)
-            else:
-                line.porcentaje_invoiced = 0.0
+            pendiente = total_oc - total_facturado
+            if pendiente > 0:
+                # Cantidad proporcional al pendiente
+                qty_pendiente = pendiente / self.price_unit
+
+                res.update({
+                    "quantity": qty_pendiente,
+                    "price_unit": self.price_unit,  # siempre el de la OC
+                })
+        return res
