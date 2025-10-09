6c27c9a0fc047a8626f124af07170293ef9f038e
Soporte <140537906+SoporteRadax01@users.noreply.github.com>
2025-10-09 09:32:41 -0600

    Update __manifest__.py

--- custom_stock_selection_report/__manifest__.py
+++ custom_stock_selection_report/__manifest__.py
@@ -8,8 +8,7 @@
         'security/ir.model.access.csv',
         'views/stock_picking_views.xml',
         'views/picking_product_select_wizard_views.xml',
-        #'reports/report.xml',
-        #'reports/product_selected_template.xml',
+        
     ],
     'installable': True,
     'application': False,
