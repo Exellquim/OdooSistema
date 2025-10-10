b3b9a117ffbd4c369c91637d40051d91f5292930
Soporte <140537906+SoporteRadax01@users.noreply.github.com>
2025-10-10 12:44:41 -0600

    Update __manifest__.py

--- hr_rotation_estado_report/__manifest__.py
+++ hr_rotation_estado_report/__manifest__.py
@@ -7,10 +7,11 @@
   'author': 'Tu Organización',
   'depends': ['hr', 'web'],
   'data': [
-    #'security/ir.model.access.csv',
+    'security/ir.model.access.csv',
     'views/employee_rotation_views.xml',
   ],
   'installable': True,
   'application': False,
 }
+
 
