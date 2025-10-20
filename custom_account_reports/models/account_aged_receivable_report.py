dac5d8e744dae79ef1429b6841316dc449689b93
Soporte <140537906+SoporteRadax01@users.noreply.github.com>
2025-10-20 11:11:26 -0600

    Update account_aged_receivable_report.py

--- custom_account_reports/models/account_aged_receivable_report.py
+++ custom_account_reports/models/account_aged_receivable_report.py
@@ -1,21 +1,31 @@
-from odoo import models
+from odoo import models, _
 
-class AccountAgedPartnerReport(models.AbstractModel):
-    _inherit = "account.aged.partner"
+class AccountAgedReceivableReport(models.AbstractModel):
+    _inherit = "account.aged.receivable.report"
 
-    def _get_columns_name(self, options):
-        columns = super()._get_columns_name(options)
-        columns.append({'name': 'Impuesto total'})
+    def _get_columns(self, options):
+        """Agregamos una columna nueva 'Impuesto total' al reporte dinámico."""
+        columns = super()._get_columns(options)
+        columns.append(
+            {
+                'name': _('Impuesto total'),
+                'class': 'number',
+                'type': 'number',
+                'sortable': True,
+            }
+        )
         return columns
 
     def _get_lines(self, options, line_id=None):
+        """Calcula el impuesto total por factura."""
         lines = super()._get_lines(options, line_id)
         for line in lines:
             move_id = line.get('id')
-            tax_amount = 0.0
+            tax_total = 0.0
             if move_id and isinstance(move_id, int):
                 move = self.env['account.move'].browse(move_id)
                 if move.move_type in ['out_invoice', 'out_refund']:
-                    tax_amount = move.amount_tax_signed
-            line['columns'].append({'name': f"{tax_amount:,.2f}"})
+                    tax_total = move.amount_tax_signed
+            # Agregar la columna al final
+            line['columns'].append({'name': tax_total, 'no_format': tax_total})
         return lines
