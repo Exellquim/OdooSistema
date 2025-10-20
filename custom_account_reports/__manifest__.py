61b3fb3ff4649c6a45a074b97d5046004a29354d
Soporte <140537906+SoporteRadax01@users.noreply.github.com>
2025-10-20 17:07:11 -0600

    Update __manifest__.py

--- custom_account_reports/__manifest__.py
+++ custom_account_reports/__manifest__.py
@@ -3,9 +3,9 @@
     'version': '1.0.0',
     'summary': 'Agrega la columna de impuesto total en el reporte de Antigüedad de Saldos',
     'description': """
-Este módulo extiende el reporte de Antigüedad de Saldos (Aged Receivable Report)
-para mostrar una columna adicional con el total de impuestos asociados
-a cada factura o partner, tanto en el reporte en pantalla como en el PDF.
+        Este módulo extiende el reporte de Antigüedad de Saldos (Aged Receivable Report)
+            para mostrar una columna adicional con el total de impuestos asociados
+            a cada factura o partner, tanto en el reporte en pantalla como en el PDF.
     """,
     'author': 'RBS-HEPL',
     'license': 'LGPL-3',
