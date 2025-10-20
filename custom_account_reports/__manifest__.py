9203ea63bb32acf418381704ff08fa7de465e1d5
Soporte <140537906+SoporteRadax01@users.noreply.github.com>
2025-10-20 11:10:21 -0600

    Update __manifest__.py

--- custom_account_reports/__manifest__.py
+++ custom_account_reports/__manifest__.py
@@ -1,8 +1,10 @@
 {
-    'name': 'Aged Receivable Report with Tax',
+    'name': 'Agregar impuestos en reporte cuenta por cobrar',
     'version': '1.0',
     'depends': ['account_reports'],
-    'data': [
-        'views/account_aged_receivable_report_inherit.xml',
-    ],
+    'author': 'RBS-HEPL',
+    'category': 'Accounting',
+    'summary': 'Agrega la columna de impuesto total al reporte de cuenta antigua por cobrar',
+    'installable': True,
+    'application': False,
 }
